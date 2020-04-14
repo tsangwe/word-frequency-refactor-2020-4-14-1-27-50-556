6fc2ad64fde4b8b2adc2901805b691fd684189f2
Wesley Tsang <tsangwe@oocl.com>
2020-04-14 16:34:51 +0800

    [Wesley] Refactor, simplified logic

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -1,4 +1,10 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.StringJoiner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class WordFrequencyGame {
 
@@ -7,50 +13,17 @@ public class WordFrequencyGame {
     private static final String CALCULATE_ERROR_MESSAGE = "Calculate Error";
 
     public String getResult(String inputString) {
-        try {
-            //split the input string with 1 to n pieces of spaces
-            String[] words = inputString.split(SPACE_PATTERN);
+        Map<String, Long> frequencyMap = Stream.of(inputString.split(SPACE_PATTERN))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        List<Input> inputs = new ArrayList<>();
+        frequencyMap.forEach((word, count) -> inputs.add(new Input(word, Math.toIntExact(count))));
+        inputs.sort((firstInput, secondInput) -> secondInput.getWordCount() - firstInput.getWordCount());
+        StringJoiner joiner = new StringJoiner(DEFAULT_DELIMITER);
+        inputs.forEach(input -> {
+            String formattedString = String.format("%s %s", input.getWord(), input.getWordCount());
+            joiner.add(formattedString);
+        });
 
-            List<Input> inputList = new ArrayList<>();
-            for (String word : words) {
-                Input input = new Input(word, 1);
-                inputList.add(input);
-            }
-
-            //get the map for the next step of sizing the same word
-            Map<String, List<Input>> map = convertToMap(inputList);
-
-            List<Input> list = new ArrayList<>();
-            for (Map.Entry<String, List<Input>> entry : map.entrySet()) {
-                Input input = new Input(entry.getKey(), entry.getValue().size());
-                list.add(input);
-            }
-            inputList = list;
-
-            inputList.sort((firstInput, secondInput) -> secondInput.getWordCount() - firstInput.getWordCount());
-
-            StringJoiner joiner = new StringJoiner(DEFAULT_DELIMITER);
-            for (Input input : inputList) {
-                String formattedString = input.getWord() + " " + input.getWordCount();
-                joiner.add(formattedString);
-            }
-            return joiner.toString();
-        } catch (Exception exception) {
-            return CALCULATE_ERROR_MESSAGE;
-        }
-    }
-
-    private Map<String, List<Input>> convertToMap(List<Input> inputList) {
-        Map<String, List<Input>> map = new HashMap<>();
-        for (Input input : inputList) {
-            if (!map.containsKey(input.getWord())) {
-                ArrayList arr = new ArrayList<>();
-                arr.add(input);
-                map.put(input.getWord(), arr);
-            } else {
-                map.get(input.getWord()).add(input);
-            }
-        }
-        return map;
+        return joiner.toString();
     }
 }
